3d10dca68d7b2d2601e329be4029b415140200ec
Francesco <francesco.mancuso@anotherweb.it>
2024-02-11 01:35:26 +0100

    Update cms15dev01.sh

--- cms15dev01.sh
+++ cms15dev01.sh
@@ -18,74 +18,77 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
 
-sudo apt -y install build-essential
-sudo apt -y install openjdk-11-jdk-headless
-sudo apt -y install fp-compiler
-sudo apt -y install postgresql
-sudo apt -y install postgresql-contrib
-sudo apt -y install postgresql-client
+sudo aptitude -y install build-essential
+sudo aptitude -y install openjdk-11-jdk-headless
+sudo aptitude -y install fp-compiler
+sudo aptitude -y install postgresql
+sudo aptitude -y install postgresql-contrib
+sudo aptitude -y install postgresql-client
 
 #for python3.10
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt -y update
-sudo apt -y install python3.10
+sudo aptitude -y install python3.10
 #for PyPy3
 sudo add-apt-repository ppa:pypy/ppa -y
 sudo apt -y update
-sudo apt -y install pypy3
+sudo aptitude -y install pypy3
 
-sudo apt -y install cppreference-doc-en-html
-sudo apt -y install cgroup-lite
+sudo aptitude -y install cppreference-doc-en-html
+sudo aptitude -y install cgroup-lite
 sudo apt -y install libcap-dev
-sudo apt -y install zip
+sudo aptitude -y install zip
 
-sudo apt -y install python3.10-dev
-sudo apt -y install libpq-dev
-sudo apt -y install libcups2-dev
-sudo apt -y install libyaml-dev
-sudo apt -y install libffi-dev
-sudo apt -y install python3-pip
+sudo aptitude -y install python3.10-dev
+sudo aptitude -y install libpq-dev
+sudo aptitude -y install libcups2-dev
+sudo aptitude -y install libyaml-dev
+sudo aptitude -y install libffi-dev
+sudo aptitude -y install python3-pip
 
-sudo apt -y install nginx-full
-sudo apt -y install python2.7
-sudo apt -y install php7.4-cli
-sudo apt -y install php7.4-fpm
-sudo apt -y install phppgadmin
-sudo apt -y install texlive-latex-base
-sudo apt -y install a2ps
+sudo aptitude -y install nginx-full
+sudo aptitude -y install python2.7
+sudo aptitude -y install php7.4-cli
+sudo aptitude -y install php7.4-fpm
+sudo aptitude -y install phppgadmin
+sudo aptitude -y install texlive-latex-base
+sudo aptitude -y install a2ps
 #sudo apt -y install gcj-jdk
-sudo apt -y install haskell-platform
-sudo apt -y install rustc
-sudo apt -y install mono-mcs
+sudo aptitude -y install haskell-platform
+sudo aptitude -y install rustc
+sudo aptitude -y install mono-mcs
 
-sudo apt -y install fp-units-base
-sudo apt -y install fp-units-fcl
-sudo apt -y install fp-units-misc
-sudo apt -y install fp-units-math
-sudo apt -y install fp-units-rtl
+sudo aptitude -y install fp-units-base
+sudo aptitude -y install fp-units-fcl
+sudo aptitude -y install fp-units-misc
+sudo aptitude -y install fp-units-math
+sudo aptitude -y install fp-units-rtl
 
-sudo apt install -y python3-setuptools
-sudo apt install -y python3-tornado
-sudo apt install -y python3-psycopg2
-sudo apt install -y python3-crypto
-sudo apt install -y python3-sqlalchemy
-sudo apt install -y python3-psutil
-sudo apt install -y python3-netifaces
-sudo apt install -y python3-pycryptodome
-sudo apt install -y python3-bs4
-sudo apt install -y python3-coverage
-sudo apt install -y python3-requests
-sudo apt install -y python3-werkzeug
-sudo apt install -y python3-gevent
-sudo apt install -y python3-bcrypt
-sudo apt install -y python3-chardet
-sudo apt install -y patool
-sudo apt install -y python3-babel
-sudo apt install -y python3-xdg
-sudo apt install -y python3-jinja2
-sudo apt install -y python3-sphinx
-sudo apt install -y python3-cups
-sudo apt install -y python3-pypdf2
+sudo aptitude install -y python3-setuptools
+sudo aptitude install -y python3-tornado
+sudo aptitude install -y python3-psycopg2
+sudo aptitude install -y python3-crypto
+sudo aptitude install -y python3-sqlalchemy
+sudo aptitude install -y python3-psutil
+sudo aptitude install -y python3-netifaces
+sudo aptitude install -y python3-pycryptodome
+sudo aptitude install -y python3-bs4
+sudo aptitude install -y python3-coverage
+sudo aptitude install -y python3-requests
+sudo aptitude install -y python3-werkzeug
+sudo aptitude install -y python3-gevent
+sudo aptitude install -y python3-bcrypt
+sudo aptitude install -y python3-chardet
+sudo aptitude install -y patool
+sudo aptitude install -y python3-babel
+sudo aptitude install -y python3-xdg
+sudo aptitude install -y python3-jinja2
+sudo aptitude install -y python3-sphinx
+sudo aptitude install -y python3-cups
+sudo aptitude install -y python3-pypdf2
+sudo aptitude -y install libcap2
+sudo aptitude -n install libcap-dev
+sudo aptitude -y install libcap-dev
 
 cd ~/
 cd olimpiadicms-bash
